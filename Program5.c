d1e9fbb0172457fa9cf9b48df454429b2dd2ef25
anantmahadik <107344574+anantmahadik@users.noreply.github.com>
2022-12-17 02:03:54 +0530

    Add files via upload

---
+++ Program5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int iNo1=0,iNo2=0;
+    printf("enter no:\n");
+    scanf("%d",&iNo1);
+
+    printf("enter no:\n");
+    scanf("%d",&iNo2);
+
+    printf("Entered First Value:%d\n",iNo1);
+    printf("Entered Second Value:%d\n",iNo2);
+    
+    return 0;
+}
